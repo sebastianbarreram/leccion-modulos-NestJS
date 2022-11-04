3e3e22a4b433d3f23bc49a58f510f516cca2e5a2
Sebastian Barrera Marin <sebastian.barrera@sofka.com.co>
2022-11-04 12:59:11 -0500

    feat: Promise and observable response

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { Observable, of } from 'rxjs';
 
 @Controller('user')
 export class UserController {
   @Get()
   getAll(@Res() response: Response): void {
     response.status(200).send("'Estos son todos los usuarios'");
+  }
+  @Get('id/:id')
+  findOne(@Param('id') id: string): string {
+    console.log(id);
+    return `This action returns a #${id} user`;
+  }
+  @Get('promise')
+  async findAll(): Promise<any[]> {
+    return [];
+  }
+  @Get('observable')
+  findAll2(): Observable<any[]> {
+    return of([]);
   }
 }
